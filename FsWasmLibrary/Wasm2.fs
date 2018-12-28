d8d536a30c29f32fb849cd4a2ac415c82190a459
Jonathan Markland <jonathan.markland@btinternet.com>
2018-12-28 10:57:13 +0000

    Renames

--- FsWasmLibrary/Wasm2.fs
+++ FsWasmLibrary/Wasm2.fs
@@ -7,32 +7,32 @@ type Export2 = { ExportName:Name; }
 type Import2 = { Export:Export2 option; ImportModuleName:Name; ImportName:Name }
 
 
-type ImportedFunction2Arg = { Import:Import2; FuncType:FuncType }
-type InternalFunction2Arg = { Export:Export2 option; OriginalCodeSecIndex:U32; CodeSize:U32; FuncType:FuncType; Locals:ValType[]; Body:Instr[] }
+type ImportedFunction2Record = { Import:Import2; FuncType:FuncType }
+type InternalFunction2Record = { Export:Export2 option; OriginalCodeSecIndex:U32; CodeSize:U32; FuncType:FuncType; Locals:ValType[]; Body:Instr[] }
 type Function2 =
-    | ImportedFunction2 of ImportedFunction2Arg
-    | InternalFunction2 of InternalFunction2Arg
+    | ImportedFunction2 of ImportedFunction2Record
+    | InternalFunction2 of InternalFunction2Record
 
 
-type ImportedGlobal2Arg = { Import:Import2; GlobalType:GlobalType }
-type InternalGlobal2Arg = { Export:Export2 option; GlobalType:GlobalType; InitExpr:Instr[] }
+type ImportedGlobal2Record = { Import:Import2; GlobalType:GlobalType }
+type InternalGlobal2Record = { Export:Export2 option; GlobalType:GlobalType; InitExpr:Instr[] }
 type Global2 =
-    | ImportedGlobal2 of ImportedGlobal2Arg
-    | InternalGlobal2 of InternalGlobal2Arg
+    | ImportedGlobal2 of ImportedGlobal2Record
+    | InternalGlobal2 of InternalGlobal2Record
 
 
-type ImportedMemory2Arg = { Import:Import2; MemoryType:MemoryType }
-type InternalMemory2Arg = { Export:Export2 option; MemoryType:MemoryType; InitData:(Instr[] * byte array)[]; }
+type ImportedMemory2Record = { Import:Import2; MemoryType:MemoryType }
+type InternalMemory2Record = { Export:Export2 option; MemoryType:MemoryType; InitData:(Instr[] * byte array)[]; }
 type Memory2 =
-    | ImportedMemory2 of ImportedMemory2Arg
-    | InternalMemory2 of InternalMemory2Arg
+    | ImportedMemory2 of ImportedMemory2Record
+    | InternalMemory2 of InternalMemory2Record
 
 
-type ImportedTable2Arg  = { Import:Import2; TableType:TableType }
-type InternalTable2Arg  = { Export:Export2 option; TableType:TableType; InitOffsetExpr:Instr[]; InitWith:FuncIdx array }
+type ImportedTable2Record  = { Import:Import2; TableType:TableType }
+type InternalTable2Record  = { Export:Export2 option; TableType:TableType; InitOffsetExpr:Instr[]; InitWith:FuncIdx array }
 type Table2 =
-    | ImportedTable2 of ImportedTable2Arg
-    | InternalTable2 of InternalTable2Arg
+    | ImportedTable2 of ImportedTable2Record
+    | InternalTable2 of InternalTable2Record
 
 
 type Module2 = {
@@ -42,4 +42,3 @@ type Module2 = {
     Tables:  Table2[];
     Globals: Global2[];
     Start:   Start option; }
-
