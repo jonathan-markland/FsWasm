be7ff5b44ca6440f7ab297af97ebc6e718e586db
Jonathan Markland <jonathan.markland@btinternet.com>
2019-01-28 19:30:49 +0000

    New file

---
+++ FsWasmLibrary/WasmBetterTypes.fs
@@ -0,0 +1,49 @@
+﻿module WasmBetterTypes
+
+open WasmFileTypes
+
+
+type Export = { ExportName:Name; }
+type Import = { Export:Export option; ImportModuleName:Name; ImportName:Name }
+
+
+
+type ImportedFunctionRecord = { Import:Import; FuncType:FuncType }
+type InternalFunctionRecord = { Export:Export option; OriginalCodeSecIndex:U32; CodeSize:U32; FuncType:FuncType; Locals:ValType[]; Body:Instr list }
+
+type ImportedGlobalRecord = { Import:Import; GlobalType:GlobalType }
+type InternalGlobalRecord = { Export:Export option; GlobalType:GlobalType; InitExpr:Instr list }
+
+type ImportedMemoryRecord = { Import:Import; MemoryType:MemoryType }
+type InternalMemoryRecord = { Export:Export option; MemoryType:MemoryType; InitData:(Instr list * byte array)[]; }
+
+type ImportedTableRecord  = { Import:Import; TableType:TableType }
+type InternalTableRecord  = { Export:Export option; TableType:TableType; InitData:(Instr list * FuncIdx array) array }
+
+
+
+type Function2 =
+    | ImportedFunction2 of ImportedFunctionRecord
+    | InternalFunction2 of InternalFunctionRecord
+
+type Global2 =
+    | ImportedGlobal2 of ImportedGlobalRecord
+    | InternalGlobal2 of InternalGlobalRecord
+
+type Memory2 =
+    | ImportedMemory2 of ImportedMemoryRecord
+    | InternalMemory2 of InternalMemoryRecord
+
+type Table2 =
+    | ImportedTable2 of ImportedTableRecord
+    | InternalTable2 of InternalTableRecord
+
+
+
+type Module2 = {
+    // NB: Decision made to drop custom sections, I could not, in general, interpret them anyway.
+    Funcs:   Function2[];
+    Mems:    Memory2[];
+    Tables:  Table2[];
+    Globals: Global2[];
+    Start:   Start option; }
