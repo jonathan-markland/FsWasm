31a71e8f0cc10be2a9bc2588321c029fb010392a
Jonathan Markland <jonathan.markland@btinternet.com>
2020-12-31 15:56:01 +0000

    Local functions

--- FsRegisterMachineLibrary/BetterWasmToJonathansAsm.fs
+++ FsRegisterMachineLibrary/BetterWasmToJonathansAsm.fs
@@ -14,74 +14,67 @@ open Library
 
 
 
-let TranslateGotoIndex (LabelName tableLabel) numMax (LabelName defaultLabel) =
-    [
-        // A is already the index to branch to
-        sprintf "cmp A,%d:if >>= goto %s" numMax defaultLabel
-        "shl A,logptr"
-        sprintf "goto [A+%s]" tableLabel
-    ]
-
-
-let TranslateCallTableIndirect () =
-    // TODO: We really need to emit some code to validate the signatures.
-    // A is already the index to call.
-    // TODO: We need to validate index A lies within wasm table [0]
-    [
-        "shl A,logptr"
-        sprintf "goto [A+%s0]" AsmTableNamePrefix  // WASM 1.0 always looks in table #0
-    ]
-
-
-
-let OfsIfNeeded u = 
-    match u with
-        | U32 0u -> ""                   // indexed addressing not needed with zero offset
-        | U32 n  -> "+" + n.ToString()   // indexed addressing needed
-
-
-let RegNameOf = function
-    | A -> "A"
-    | B -> "B"
-    | C -> "C"
-    | Y -> "Y"
-
-
-let TranslateREGU32 s1 r u s2 = 
-    [ sprintf "%s%s%s%s" s1 (RegNameOf r) (OfsIfNeeded u) s2 ]
-
-
-let TranslateREGU32I32 s1 r u s2 n = 
-    [ sprintf "%s%s%s%s%d" s1 (RegNameOf r) (OfsIfNeeded u) s2 n ]
-
-
-
 let TranslateInstructionToAsmSequence instruction =
 
     // TODO:  These translations can assume a 32-bit target for now.
+
+    let regNameOf = function
+        | A -> "A"
+        | B -> "B"
+        | C -> "C"
+        | Y -> "Y"
+
+    let offsetIfNeeded u = 
+        match u with
+            | U32 0u -> ""                   // indexed addressing not needed with zero offset
+            | U32 n  -> "+" + n.ToString()   // indexed addressing needed
+
+    let translateREGU32 s1 r u s2 = 
+        [ sprintf "%s%s%s%s" s1 (regNameOf r) (offsetIfNeeded u) s2 ]
+
+    let translateREGU32I32 s1 r u s2 n = 
+        [ sprintf "%s%s%s%s%d" s1 (regNameOf r) (offsetIfNeeded u) s2 n ]
+
+    let translateGotoIndex (LabelName tableLabel) numMax (LabelName defaultLabel) =
+        [
+            // A is already the index to branch to
+            sprintf "cmp A,%d:if >>= goto %s" numMax defaultLabel
+            "shl A,logptr"
+            sprintf "goto [A+%s]" tableLabel
+        ]
+
+    let translateCallTableIndirect () =
+        // TODO: We really need to emit some code to validate the signatures.
+        // A is already the index to call.
+        // TODO: We need to validate index A lies within wasm table [0]
+        [
+            "shl A,logptr"
+            sprintf "goto [A+%s0]" AsmTableNamePrefix  // WASM 1.0 always looks in table #0
+        ]
+
 
     match instruction with
         | Barrier               -> [ "// ~~~ register barrier ~~~" ]
         | Breakpoint            -> [ "break" ]
         | Drop                  -> [ "add SP,4" ]  // TODO: Assumes 32-bit target
         | Label(LabelName l)    -> [ "label " + l ]   // TODO: sort out using the local label references in Jonathan's ASM
-        | Const(r,Const32(n))   -> [ sprintf "let %s=%d" (RegNameOf r) n ]
+        | Const(r,Const32(n))   -> [ sprintf "let %s=%d" (regNameOf r) n ]
         | Goto(LabelName l)     -> [ "goto " + l ]
         | CallFunc(LabelName l) -> [ "call " + l ]
-        | CallTableIndirect     -> TranslateCallTableIndirect ()
+        | CallTableIndirect     -> translateCallTableIndirect ()
         | BranchAZ(LabelName l) -> [ "cmp A,0:if z goto " + l ]
         | BranchANZ(LabelName l)-> [ "cmp A,0:if nz goto " + l ]
-        | GotoIndex(t,n,d,_)    -> TranslateGotoIndex t n d   // The ignored parameter is the lookup table, which we separately output.
-        | Push(r)               -> [ sprintf "push %s" (RegNameOf r) ]
-        | Pop(r)                -> [ sprintf "pop %s" (RegNameOf r) ]
+        | GotoIndex(t,n,d,_)    -> translateGotoIndex t n d   // The ignored parameter is the lookup table, which we separately output.
+        | Push(r)               -> [ sprintf "push %s" (regNameOf r) ]
+        | Pop(r)                -> [ sprintf "pop %s" (regNameOf r) ]
         | PeekA                 -> [ "let A=int [SP]" ]  // TODO: Assumes 32-bit target
-        | Let(r1,r2)            -> [ sprintf "let %s=%s" (RegNameOf r1) (RegNameOf r2) ]
+        | Let(r1,r2)            -> [ sprintf "let %s=%s" (regNameOf r1) (regNameOf r2) ]
         | AddAN(I32(n))         -> [ sprintf "add A,%d" n ]
         | SubAN(I32(n))         -> [ sprintf "sub A,%d" n ]
         | AndAN(I32(n))         -> [ sprintf "and A,%d" n ]
         | OrAN(I32(n))          -> [ sprintf "or A,%d"  n ]
         | XorAN(I32(n))         -> [ sprintf "xor A,%d" n ]
-        | Add(r1,r2)            -> [ sprintf "add %s,%s" (RegNameOf r1) (RegNameOf r2) ]  // commutative
+        | Add(r1,r2)            -> [ sprintf "add %s,%s" (regNameOf r1) (regNameOf r2) ]  // commutative
         | SubBA                 -> [ "sub B,A" ]
         | MulAB                 -> [ "mul A,B" ]  // commutative
         | DivsBA | DivuBA | RemsBA | RemuBA -> failwith "Assembler does not have division or remainder instructions"
@@ -103,21 +96,21 @@ let TranslateInstructionToAsmSequence instruction =
         | CmpGesBA              -> [ "cmp B,A:set >= A" ]
         | CmpGeuBA              -> [ "cmp B,A:set >>= A" ]
         | CmpAZ                 -> [ "cmp A,0:set z A" ]
-        | FetchLoc(r,i)         -> [ sprintf "let %s=int[@%s]" (RegNameOf r) (LocalIdxNameString i) ]  // TODO: Assumes 32-bit target
-        | StoreLoc(r,i)         -> [ sprintf "let int[@%s]=%s" (LocalIdxNameString i) (RegNameOf r) ]  // TODO: Assumes 32-bit target
-        | FetchGlo(r,i)         -> [ sprintf "let %s=int[%s]" (RegNameOf r) (GlobalIdxNameString i) ]  // TODO: Eventually use the type rather than "int"
-        | StoreGlo(r,i)         -> [ sprintf "let int[%s]=%s" (GlobalIdxNameString i) (RegNameOf r) ]  // TODO: Eventually use the type rather than "int"
-        | StoreConst8(r,ofs,I32(v))   -> TranslateREGU32I32 "let byte[" r ofs "]=" v
-        | StoreConst16(r,ofs,I32(v))  -> TranslateREGU32I32 "let ushort[" r ofs "]=" v
-        | StoreConst32(r,ofs,I32(v))  -> TranslateREGU32I32 "let uint[" r ofs "]=" v  
-        | Store8A(r,ofs)       -> TranslateREGU32 "let byte[" r ofs "]=A"  
-        | Store16A(r,ofs)      -> TranslateREGU32 "let ushort[" r ofs "]=A"
-        | Store32A(r,ofs)      -> TranslateREGU32 "let uint[" r ofs "]=A"  
-        | Fetch8s(r,ofs)       -> TranslateREGU32 "let A=sbyte[" r ofs "]" 
-        | Fetch8u(r,ofs)       -> TranslateREGU32 "let A=byte[" r ofs "]"  
-        | Fetch16s(r,ofs)      -> TranslateREGU32 "let A=short[" r ofs "]" 
-        | Fetch16u(r,ofs)      -> TranslateREGU32 "let A=ushort[" r ofs "]"
-        | Fetch32(r,ofs)       -> TranslateREGU32 "let A=uint[" r ofs "]"  
+        | FetchLoc(r,i)         -> [ sprintf "let %s=int[@%s]" (regNameOf r) (LocalIdxNameString i) ]  // TODO: Assumes 32-bit target
+        | StoreLoc(r,i)         -> [ sprintf "let int[@%s]=%s" (LocalIdxNameString i) (regNameOf r) ]  // TODO: Assumes 32-bit target
+        | FetchGlo(r,i)         -> [ sprintf "let %s=int[%s]" (regNameOf r) (GlobalIdxNameString i) ]  // TODO: Eventually use the type rather than "int"
+        | StoreGlo(r,i)         -> [ sprintf "let int[%s]=%s" (GlobalIdxNameString i) (regNameOf r) ]  // TODO: Eventually use the type rather than "int"
+        | StoreConst8(r,ofs,I32(v))   -> translateREGU32I32 "let byte[" r ofs "]=" v
+        | StoreConst16(r,ofs,I32(v))  -> translateREGU32I32 "let ushort[" r ofs "]=" v
+        | StoreConst32(r,ofs,I32(v))  -> translateREGU32I32 "let uint[" r ofs "]=" v  
+        | Store8A(r,ofs)       -> translateREGU32 "let byte[" r ofs "]=A"  
+        | Store16A(r,ofs)      -> translateREGU32 "let ushort[" r ofs "]=A"
+        | Store32A(r,ofs)      -> translateREGU32 "let uint[" r ofs "]=A"  
+        | Fetch8s(r,ofs)       -> translateREGU32 "let A=sbyte[" r ofs "]" 
+        | Fetch8u(r,ofs)       -> translateREGU32 "let A=byte[" r ofs "]"  
+        | Fetch16s(r,ofs)      -> translateREGU32 "let A=short[" r ofs "]" 
+        | Fetch16u(r,ofs)      -> translateREGU32 "let A=ushort[" r ofs "]"
+        | Fetch32(r,ofs)       -> translateREGU32 "let A=uint[" r ofs "]"  
         | ThunkIn              -> 
             // The translated code requires the Y register to
             // point to the base of the linear memory region.
@@ -196,11 +189,6 @@ let WriteOutFunctionLocals writeOut (funcType:FuncType) funcLocals =
 
 
 
-let ReturnCommandFor (funcType:FuncType) (funcLocals:ValType[]) =
-    match (funcType.ParameterTypes.Length, funcLocals.Length) with
-        | (0,0) -> "ret"
-        | (_,_) -> "endproc"
-
 
 
 
@@ -247,11 +235,16 @@ let WriteOutFunctionAndBranchTables writeOutCode writeOutTables funcIndex (m:Mod
     let branchTableItem targetLabel =
         writeOutTables (sprintf "    ptr %s" targetLabel)
 
+    let returnCommandFor (funcType:FuncType) (funcLocals:ValType[]) =
+        match (funcType.ParameterTypes.Length, funcLocals.Length) with
+            | (0,0) -> "ret"
+            | (_,_) -> "endproc"
+
     try
         writeOutCode procedureCommand
         WriteOutFunctionLocals writeOutCode f.FuncType f.Locals
         crmInstructions |> ForTranslatedCrmInstructionsDo writeInstruction TranslateInstructionToAsmSequence f.FuncType config
-        writeOutCode (ReturnCommandFor f.FuncType f.Locals)
+        writeOutCode (returnCommandFor f.FuncType f.Locals)
         crmInstructions |> ForAllBranchTablesDo branchTableStart branchTableItem
     with
         | _ as ex -> failwith (sprintf "Error in %s:  %s" procedureCommand (ex.ToString()))
